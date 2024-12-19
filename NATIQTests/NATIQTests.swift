66aa8f2ce96b100b9241a934f35165a26e9a46d2
Walaa <Walaa.matar20@gmail.com>
2024-12-19 16:03:57 +0300

    Initial Commit

---
+++ NATIQTests/NATIQTests.swift
@@ -0,0 +1,17 @@
+//
+//  NATIQTests.swift
+//  NATIQTests
+//
+//  Created by Walaa on 19/12/2024.
+//
+
+import Testing
+@testable import NATIQ
+
+struct NATIQTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
